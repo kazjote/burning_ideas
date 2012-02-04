e0ce419b8d8f9ba9664925327e7aea150c0fac70
Kacper Bielecki <kazjote@gmail.com>
2012-02-04 12:10:34 +0100

    Written first backend functional tests

--- backend/test/burning_ideas/test/core.clj
+++ backend/test/burning_ideas/test/core.clj
@@ -2,3 +2,7 @@
   (:use [burning_ideas.core])
   (:use [clojure.test]))
 
+(deftest root-path-redirection
+  (let [response (main-routes {:request-method :get :uri "/"})]
+    (is (= 301 (:status response)))
+    (is (= "/index.html" (get (:headers response) "Location")))))
--- backend/test/burning_ideas/test/ideas.clj
+++ backend/test/burning_ideas/test/ideas.clj
@@ -7,16 +7,16 @@
   {:last-reset (clj-time/minus (clj-time/now) interval)})
 
 (deftest determine-current-hotness
-  (let [idea (idea-reset-in-past (clj-time/minutes 1))]
-    (is 99 (current-hotness idea)))
+  (let [idea (idea-reset-in-past (clj-time/secs 1))]
+    (is (= 99 (current-hotness idea))))
 
   (let [idea (idea-reset-in-past (clj-time/days 1))]
-    (is 0 (current-hotness idea))))
+    (is (= 0 (current-hotness idea)))))
 
 (deftest modifying-hotness
   (let [idea (idea-reset-in-past (clj-time/days 1))]
     (swap! ideas-hash assoc 1 idea)
     (let [returned-idea (modify-hotness 1)]
-      (is 100 (current-hotness returned-idea))
-      (is 100 (current-hotness (find-idea 1))))))
+      (is (= 100 (current-hotness returned-idea)))
+      (is (= 100 (current-hotness (find-idea 1)))))))
 
